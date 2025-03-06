84888a22a88c16eda462648abdf811e54fc9d4ef
Maycol Andres <151478637+Maycol-2018@users.noreply.github.com>
2025-03-06 00:24:50 -0500

    New changes

--- src/main/scala/Multiplicacion/package.scala
+++ src/main/scala/Multiplicacion/package.scala
@@ -13,11 +13,11 @@ package object Multiplicacion {
     }
   }
 
-  def PeasantAlgorithmV2(x: Int, y: Int, acc: Int = 0): Int = {
+  def PeasantAlgorithmIt(x: Int, y: Int, acc: Int = 0): Int = {
     // Version iterativa lineal del Peasant Algorithm para multiplicar dos enteros positivos
     if (x == 0) acc
-    else if (x % 2 == 0) PeasantAlgorithmV2(x / 2, y * 2, acc) // Si x es par, dividir y multiplicar
-    else PeasantAlgorithmV2(x / 2, y * 2, acc + y) // Si x es impar, acumular y
+    else if (x % 2 == 0) PeasantAlgorithmIt(x / 2, y * 2, acc) // Si x es par, dividir y multiplicar
+    else PeasantAlgorithmIt(x / 2, y * 2, acc + y) // Si x es impar, acumular y
   }
   
   
